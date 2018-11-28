91914ac06e910894b444a4b87da7e30d8e0f3a00
Wojtek <wojtek.luszczynski@dcx.bz>
2018-11-28 11:41:12 +0100

    Poprawa tekstu zadania 4.2

--- W_wyjatkowej_sieci_z_plikami/praca_domowa/zadanie_2.py
+++ W_wyjatkowej_sieci_z_plikami/praca_domowa/zadanie_2.py
@@ -4,14 +4,15 @@ Okazało się że firmowa baza danych klientów wyciekła do internetu.
 
 By firma ConfWorld dalej funkcjonowała musisz przekazać klientom
 informacje na temat wycieku, ale nie chcesz siać paniki.
-W pierwszej kolejności należy zresetować hasła klientom którzy nie zmieniali hasła od czasu ataku
+W pierwszej kolejności należy zresetować hasła klientom którzy nie zmieniali hasła od czasu ataku i mają słabe hasło
+(figurujące na liście słabych haseł)
 
 Napisz program by znaleźć takie osoby. Wyciek danych nastąpił 10.11.2017 w samo południe
 Twoja baza danych jest aktualna (załóżmy dzień 20.11.2018 00:00)
 
 Na domiar złego okazało się że Twoi klienci używali bardzo słabych haseł.
 By w przyszłości nikt nie łamał haseł Twoich klientów,
-wykożystując listę dancyh najczęściej używanych haseł w internecie,
+wykorzystując listę danych najczęściej używanych haseł w internecie,
 napisz kolejną część programu który w 2-giej kolejności:
     znajdzie wszystkich użytkowników którzy mają hasło figurujące na tej liście
     znajdzie top 10 występujących haseł w serwisie figurujących na liście top500 w PLAIN TEXT
@@ -22,30 +23,73 @@ Dane:
     hasła w bazie są hashowane algorytmem SHA1
     lista 500 najczęstszych haseł w internecie -> top_500_most_common_passwords.txt
     baza danych która wyciekła do internetu   -> confwrld_user_data_dump_10.11.2017.csv
-    aktualna baza klientów                    -> confwrld_user_database.csv
+    aktualna baza klientów                               -> confwrld_user_database.csv
     wszystkie daty są z tej samej strefy czasowej
 
 Wymagania szczegółowe:
     ConfData
-        Struktura przechwująca dane
-    ConfData.weak_pass_users(pass_list)
-        Zwraca listę ludzi którzy mają słabe hasło figurujące na liście słaych haseł
-        pass_list = [["pass1", "pass1_hash"], ["pass1", "pass1_hash"], ...]
-    ConfData.common_pass(pass_list, n=10) = ["pass1", "pass2", ..... ]
-        Zwraca listę ludzi którzy mają najczęściej występujące hasła
-        pass_list = [["pass1", "pass1_hash"], ["pass1", "pass1_hash"], ...]
-    ConfData.pwned_users(pass_list, hack_time) = ["mail1", "mail2", ..... ]
-        Zwraca listę ludzi których musimy poinformować o wycieku ich hasła
-        hack_time -> objekt Datetime reprezentujący czas wycieku
-    ConfData.safe_users(pass_list, hack_time) = ["mail1", "mail2", ..... ]
-        Zwraca listę ludzi którzy nie figurowali w wycieku i mają bezpieczne hasła
-        hack_time -> objekt Datetime reprezentujący czas wycieku
+        Struktura przechowująca dane (dowolnie)
 
-Do dyzpozycji jest tylko próbka danych które firma ConfWord przekazała do analizy
-Napisz program tak by działał dla dowolych danych i czasu wycieku
+    ConfData.weak_pass_users(pass_list)
+        Przyjmuje:
+            pass_list -> struktura z wczytanymi hasłami i jej hashami
+        Zwraca:
+            ["mail1", "mail2", ..... ]
+            Zwraca listę ludzi którzy mają słabe hasło figurujące na liście słabych haseł
+
+    ConfData.common_pass(pass_list, n=10)
+        Przyjmuje:
+            pass_list -> struktura z wczytanymi hasłami i jej hashami
+            n -> liczba haseł jakie chcemy otrzymać
+        Zwraca:
+            [["pass1", "pass1_hash"], ["pass1", "pass1_hash"], ...]
+            listę list haseł i ich hashy SHA1
+
+    ConfData.pwned_users(pass_list, hack_time)
+        Przyjmuje:
+            pass_list -> struktura z wczytanymi hasłami i jej hashami
+            hack_time -> obiekt Datetime reprezentujący czas wycieku
+        Zwraca:
+            ["mail1", "mail2", ..... ]
+            listę ludzi których musimy poinformować o wycieku ich hasła (tylko tych ze słabymi hasłami)
+
+    ConfData.safe_users(pass_list, hack_time)
+        Przyjmuje:
+            pass_list -> struktura z wczytanymi hasłami i jej hashami
+            hack_time -> obiekt Datetime reprezentujący czas wycieku
+        Zwraca:
+            ["mail1", "mail2", ..... ]
+            listę ludzi którzy nie figurowali w wycieku i mają bezpieczne hasła
+
+Do dyspozycji jest tylko próbka danych które firma ConfWord przekazała do analizy
+Napisz program tak by działał dla dowolnych danych i czasu wycieku
 
 '''
+import hashlib
+import csv
+from collections import defaultdict, OrderedDict
+from datetime import datetime
 
 
 class ConfData():
-    pass
+
+    # Najważniejsze funkcje (ale nie wszystkie !) które trzeba uzupełnić
+
+    def __init__(self, database_file):
+        pass
+
+    def weak_pass_users(self, pass_list):
+        pass
+
+    def common_pass(self, pass_list, n=10):
+        pass
+
+    def pwned_users(self, pass_list, hack_time):
+        pass
+
+    def safe_users(self, pass_list, hack_time):
+        pass
+
+    @staticmethod
+    def read_passwords(database_file):
+        pass
